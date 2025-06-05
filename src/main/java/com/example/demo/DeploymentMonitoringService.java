91458ea8dae3558bb94fecf9bf0a3eb7e8cca593
nasonawa <nasonawa@redhat.com>
2025-06-05 23:00:32 +0530

    added autowired

--- src/main/java/com/example/demo/DeploymentMonitoringService.java
+++ src/main/java/com/example/demo/DeploymentMonitoringService.java
@@ -6,13 +6,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DeploymentMonitoringService {
-
-    private final OpenShiftClient openShiftClient;
-
+    
     @Autowired
+    private OpenShiftClient openShiftClient;
+
+    /*@Autowired
     public DeploymentMonitoringService(OpenShiftClient openShiftClient) {
         this.openShiftClient = openShiftClient;
-    }
+    }*/
 
     public void listDeployments() {
         // Example: List all deployments in the default namespace
@@ -23,12 +24,4 @@ public class DeploymentMonitoringService {
                 .forEach(dc -> System.out.println("Deployment Config: " + dc.getMetadata().getName()));
     }
 
-    public void scaleDeployment(String deploymentName, int replicas) {
-        // Example: Scale a deployment
-        openShiftClient.deploymentConfigs()
-                .inNamespace("default")
-                .withName(deploymentName)
-                .scale(replicas);
-        System.out.println("Scaled " + deploymentName + " to " + replicas + " replicas");
-    }
 }
